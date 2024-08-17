3eeb73eb575c297b2c91bf1b897ff256a13358c9
rtg <rtg1539@naver.com>
2024-08-17 22:37:57 +0900

    initial commit

---
+++ src/main/java/sesac/sesaccdemo/SesaccDemoApplication.java
@@ -0,0 +1,13 @@
+package sesac.sesaccdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesaccDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SesaccDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sesac/sesaccdemo/SesaccDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sesac.sesaccdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SesaccDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
