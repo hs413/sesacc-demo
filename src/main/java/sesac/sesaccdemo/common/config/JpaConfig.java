5a5f08cc121f41ad17f6a9d567571a1071670053
rtg <rtg1539@naver.com>
2024-08-18 20:52:20 +0900

    feat: JpaConfig

---
+++ src/main/java/sesac/sesaccdemo/common/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package sesac.sesaccdemo.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
